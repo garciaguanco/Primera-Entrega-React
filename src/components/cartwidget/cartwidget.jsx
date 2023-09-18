02b1c144f4eab7a1c9cfbb5f6e55aefbbcf83772
sbeastian garcia <garciaguanco@icloud.com>
2023-09-18 18:06:04 -0300

    subo pre entrega 1

---
+++ src/components/cartwidget/cartwidget.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CartWidget = () => {
+  return (
+    <div className="ml-autok">
+      <a to="/Cart" className="navbar-brand" href="#">
+        <i className="fas fa-shopping-cart" style={{ color: "black" }}>
+          <span className="span-icon" style={{ color: "black" }}>
+            0
+          </span>
+        </i>
+      </a>
+    </div>
+  );
+};
+export default CartWidget;
