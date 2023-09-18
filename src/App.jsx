b2692f574cb225e3e50f7c71914794d898ed9c62
sbeastian garcia <garciaguanco@icloud.com>
2023-09-17 22:08:57 -0300

    subo repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card From './components/counter/countercontainer'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/cartwidget/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const cartwidget = () => {
+    return( 
+        <i className="bi bi-cart"></i>
+    );      expression expected.
+}
+
+export default cartwidget
+
